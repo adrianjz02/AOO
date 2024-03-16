b203bd576ef3ef40cf9d70fe6ec220115e525aa8
angelhu.pro@gmail.com <angelhu.pro@gmail.com>
2024-03-16 15:38:21 +0100

    Connexion avec la base de données

---
+++ src/main/java/jo/demo/DBConnect.java
@@ -0,0 +1,24 @@
+package jo.demo;
+import java.sql.*;
+
+public class DBConnect {
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/dbconnect", "root", ""
+            );
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from students");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3));
+                ;
+
+            }
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
